a26aa7b9b0d1d9571f4e2a6037ec2b8f97658b21
ElenaVeselovskaya <611717Elena>
2019-10-11 11:29:15 +0300

    fixes #2 Updated pom.xml fixes #3 Deleted else condition fixes #4 Deleted unnecessary field

--- src/main/java/TaxiService.java
+++ src/main/java/TaxiService.java
@@ -1,7 +1,5 @@
 public class TaxiService {
-    int taxiBoardingAmountInRub;
-    int fareForOneKmInRub;
-    int distanceInKm;
+
     int amountOfOrderForDiscountInRub = 1_000;
     int fivePerSentDiscount = 5;
     int hundredPerSent = 100;
@@ -13,11 +11,10 @@ public class TaxiService {
         int amountOfDiscountInRub = amountOfOrderInRub * fivePerSentDiscount / hundredPerSent;
 
         if (amountOfOrderInRub > amountOfOrderForDiscountInRub & amountOfDiscountInRub > limitOfDiscountInRub) {
-            return (amountOfOrderInRub - limitOfDiscountInRub);
-        } else if (amountOfOrderInRub > amountOfOrderForDiscountInRub & amountOfDiscountInRub < limitOfDiscountInRub) {
+            return (amountOfOrderInRub - limitOfDiscountInRub); }
+        if (amountOfOrderInRub > amountOfOrderForDiscountInRub & amountOfDiscountInRub < limitOfDiscountInRub) {
             return (amountOfOrderInRub - amountOfDiscountInRub);
-        } else {
-            return amountOfOrderInRub;
+        }
+        return amountOfOrderInRub;
         }
     }
-}
